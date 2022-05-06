e1af50026f733f89ea72ed89fc153d4c1e78fede
Eliseo Gonzalez <eliseog@herbalife.com>
2022-05-06 14:43:58 -0500

    Add project files.

---
+++ BlazorSignalR/Server/TextHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalR.Server
+{
+    public class TextHub : Hub
+    {
+        public async Task Send(string text)
+        {
+            await Clients.All.SendAsync("ReceiveInformation", text);
+        }
+    }
+}
